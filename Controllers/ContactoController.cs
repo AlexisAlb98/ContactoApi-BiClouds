07697a67907bd3a043b35a9656009418ace15a7f
AlexisAlb98 <alexisalbarracin2014@gmail.com>
2024-09-13 21:13:22 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/ContactoController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ContactoApi.Models;
+using System.Data.SqlClient;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ContactoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactoController : ControllerBase
+    {
+        string connectionString = "Server=BDTesting.mssql.somee.com; Initial Catalog=BDTesting; User ID=matiaschamarez_SQLLogin_1; Password=xqqx9qyuey; Persist Security Info=False; TrustServerCertificate=True;";
+
+
+        [HttpGet]
+        [Route("GetContactos")]
+        public List<Contacto> GetContactos()
+        {
+            List<Contacto> lc = new List<Contacto>();
+
+            string Query= "Select IdContacto,NombreCompleto,Telefono,Mail,Mensaje,FechaEnvioMensaje,Leido from Contacto\r\n";
+
+            SqlConnection sqlConn = new SqlConnection(connectionString);
+            sqlConn.Open();
+            SqlCommand sqlCm = new SqlCommand(Query, sqlConn);
+            SqlDataReader dr = sqlCm.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Contacto contacto = new Contacto();
+                contacto.IdContacto = int.Parse(dr[0].ToString());
+                contacto.NombreCompleto = dr[1].ToString();
+                contacto.Telefono = dr[2].ToString();
+                contacto.Mail = dr[3].ToString();
+                contacto.Mensaje = dr[4].ToString();
+                contacto.FechaEnvioMensaje = dr[5].ToString();
+                var valorLeido = dr[6].ToString();
+                contacto.Leido = bool.TryParse(valorLeido, out bool resultado) ? resultado : false;
+                lc.Add(contacto);
+            }
+
+            sqlConn.Close();
+
+            return lc; 
+            
+        }
+   
+    }
+}
---
+++ Models/Contacto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactoApi.Models
+{
+    public class Contacto
+    {
+        public int IdContacto { get; set; }
+
+        [Required(ErrorMessage = "El nombre completo es obligatorio.")]
+        public string NombreCompleto { get; set; }
+
+        [Phone(ErrorMessage = "El número de teléfono no es válido.")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El correo electrónico no es válido.")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "El mensaje es obligatorio.")]
+        public string Mensaje { get; set; }
+
+        public string FechaEnvioMensaje { get; set; }
+
+        public bool Leido {  get; set; }
+
+    }
+}
